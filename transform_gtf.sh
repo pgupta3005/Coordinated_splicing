25d865fcbd7453d4920acc22925791d20355b262
Pallavi Gupta <84703808+pgupta3005@users.noreply.github.com>
2023-11-20 16:13:13 +1000

    Add files via upload

---
+++ transform_gtf.sh
@@ -0,0 +1,17 @@
+!#!/bin/bash
+
+source ~/miniconda3/bin/activate env1
+
+INPUT_DIR=$PWD
+OUTPUT_DIR=$PWD
+
+while read i
+do
+gffread -E --keep-genes ${i}.collapsed_classification.filtered_lite.gtf -o- > ${i}_1.gtf
+docker run --ipc=host --gpus all \
+-v "${INPUT_DIR}":"${INPUT_DIR}" \
+-v "${OUTPUT_DIR}":"${OUTPUT_DIR}" \
+quay.io/biocontainers/agat:0.8.0--pl5262hdfd78af_0 agat_convert_sp_gff2gtf.pl \
+--in "${INPUT_DIR}"/${i}_1.gtf --out "${OUTPUT_DIR}"/${i}_2.gtf
+sed 's/ID/exon_id/g' ${i}_2.gtf > ${i}_3.gtf
+done < filenames
